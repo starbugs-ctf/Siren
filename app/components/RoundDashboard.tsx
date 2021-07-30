fa219d3079feb183ac4dc4e2997c333b5a267833
Yechan Bae <yechan@gatech.edu>
2021-07-30 18:04:23 -0400

    More CSS for RoundDashboard

--- app/components/RoundDashboard.tsx
+++ app/components/RoundDashboard.tsx
@@ -1,5 +1,7 @@
 import { useQuery } from "blitz"
 import getAllTeams from "app/queries/getAllTeams"
+import { CheckIcon, XIcon } from "@heroicons/react/solid"
+import { PuffLoader } from "react-spinners"
 
 type RoundDashboardProps = {
   round: number
@@ -9,41 +11,50 @@ export const RoundDashboard = (props: RoundDashboardProps) => {
   const [teams] = useQuery(getAllTeams, null)
 
   return (
-    <>
-      <h1>Round {props.round}</h1>
-      <table className="dashboard">
-        <thead>
-          <tr>
-            <th></th>
-            <th className="rotate">
-              <div>
-                <span>Long column 1</span>
-              </div>
-            </th>
-            <th className="rotate">
-              <div>
-                <span>Long column 2</span>
-              </div>
-            </th>
-            <th className="rotate">
-              <div>
-                <span>Short</span>
-              </div>
-            </th>
+    <table className="dashboard">
+      <thead>
+        <tr>
+          <th>Round 100</th>
+          <th className="rotate">
+            <div>
+              <span>Long column 1</span>
+            </div>
+          </th>
+          <th className="rotate">
+            <div>
+              <span>Long column 2</span>
+            </div>
+          </th>
+          <th className="rotate">
+            <div>
+              <span>Short</span>
+            </div>
+          </th>
+          <th className="rotate">
+            <div>
+              <span>Problem</span>
+            </div>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {teams.map((team) => (
+          <tr key={team.slug}>
+            <td className="team">{team.name}</td>
+            <td className="cell okay">
+              <CheckIcon className="h-5 w-5 text-green-600" />
+            </td>
+            <td className="cell fail">
+              <XIcon className="h-5 w-5 text-red-600" />
+            </td>
+            <td className="cell">
+              <PuffLoader size="20px" color="#999" />
+            </td>
+            <td className="cell">-</td>
           </tr>
-        </thead>
-        <tbody>
-          {teams.map((team) => (
-            <tr key={team.slug}>
-              <td className="team">{team.name}</td>
-              <td className="cell">A</td>
-              <td className="cell">B</td>
-              <td className="cell">C</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
+        ))}
+      </tbody>
+    </table>
   )
 }
 
