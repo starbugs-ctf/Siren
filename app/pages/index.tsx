abe5760cd2db1241dcb2391b360c751da8cf4701
Yechan Bae <yechan@gatech.edu>
2021-08-01 20:53:52 -0400

    Implement current round calculation

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,9 +1,124 @@
-import { BlitzPage } from "blitz"
+import { BlitzPage, useQuery } from "blitz"
+import { useEffect, useState } from "react"
+import { format, formatDistance } from "date-fns"
+import { Round } from "db"
+
 import Layout from "app/core/layouts/Layout"
 import RoundDashboard from "app/components/RoundDashboard"
 import { RoundTaskList } from "app/components/RoundTaskList"
+import getAllRoundRanges from "app/queries/getAllRoundRanges"
+import getAllRounds from "app/queries/getAllRounds"
+import { getRoundDuration, RoundDuration } from "app/roundUtil"
+
+const HOUR_FORMAT = "MMM dd HH:mm:ss"
+
+type RoundData = {
+  round: Round
+  duration: RoundDuration
+}
+
+type RoundNowProps = {
+  lastRound: RoundData | null
+  nextRound: RoundData | null
+}
+
+const RoundNow = ({ lastRound, nextRound }: RoundNowProps) => {
+  const [now, setNow] = useState(new Date())
+
+  useEffect(() => {
+    const interval = setInterval(() => setNow(new Date()), 1000)
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
+
+  const OPT = {
+    addSuffix: true,
+    includeSeconds: true,
+  }
+
+  if (lastRound === null && nextRound === null) {
+    return (
+      <div className="round-now">
+        <h2>{format(now, HOUR_FORMAT)}</h2>
+        Please initialize the database
+      </div>
+    )
+  } else if (lastRound === null) {
+    return (
+      <div className="round-now">
+        <h2>{format(now, HOUR_FORMAT)}</h2>
+        Waiting for the contest to start ({formatDistance(nextRound!.duration.end, now, OPT)})
+      </div>
+    )
+  } else {
+    if (lastRound.duration.start <= now && now <= lastRound.duration.end) {
+      return (
+        <div className="round-now">
+          <h2>{format(now, HOUR_FORMAT)}</h2>
+          Round {lastRound.round.id} is running ({formatDistance(lastRound.duration.end, now, OPT)})
+        </div>
+      )
+    } else if (nextRound) {
+      return (
+        <div className="round-now">
+          <h2>{format(now, HOUR_FORMAT)}</h2>
+          Waiting for round {nextRound.round.id} (
+          {formatDistance(nextRound.duration.start, now, OPT)})
+        </div>
+      )
+    } else {
+      return (
+        <div className="round-now">
+          <h2>{format(now, HOUR_FORMAT)}</h2>
+          The competition has finished
+        </div>
+      )
+    }
+  }
+}
 
 const Home: BlitzPage = () => {
+  const [rounds] = useQuery(getAllRounds, null)
+  const [roundRanges] = useQuery(getAllRoundRanges, null)
+
+  const roundData = rounds.map((round) => {
+    return {
+      round,
+      duration: getRoundDuration(roundRanges, round.id),
+    }
+  })
+
+  const now = new Date()
+
+  let lastRound: null | RoundData = null
+  let nextRound: null | RoundData = null
+
+  for (const current of roundData) {
+    if (current.duration.start <= now) {
+      if (lastRound === null || current.duration.start >= lastRound.duration.start) {
+        lastRound = current
+      }
+    } else {
+      if (nextRound === null || current.duration.start <= nextRound.duration.start) {
+        nextRound = current
+      }
+    }
+  }
+
+  if (lastRound === null) {
+    return (
+      <div className="card">
+        <div className="card-title">
+          <h1>Dashboard</h1>
+        </div>
+        <div className="card-body">
+          <RoundNow lastRound={lastRound} nextRound={nextRound} />
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className="flex flex-wrap flex-column gap-8">
       <div className="flex-auto">
@@ -12,7 +127,8 @@ const Home: BlitzPage = () => {
             <h1>Dashboard</h1>
           </div>
           <div className="card-body">
-            <RoundDashboard round={100} />
+            <RoundNow lastRound={lastRound} nextRound={nextRound} />
+            <RoundDashboard round={lastRound.round.id} />
           </div>
         </div>
       </div>
@@ -22,7 +138,7 @@ const Home: BlitzPage = () => {
             <h1>Tasks</h1>
           </div>
           <div className="card-body">
-            <RoundTaskList round={100} />
+            <RoundTaskList round={lastRound.round.id} />
           </div>
         </div>
       </div>
