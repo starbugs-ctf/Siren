15483bb3bf879377690ee5ee18eca7b3e2f20383
Yechan Bae <yechan@gatech.edu>
2021-08-04 21:35:27 -0400

    Implement endpoint API

---
+++ app/api/endpoint.tsx
@@ -0,0 +1,47 @@
+import { handleError } from "app/apiUtil"
+import { BlitzApiRequest, BlitzApiResponse, BlitzApiHandler } from "blitz"
+import db from "db"
+import * as z from "zod"
+
+const EndpointSchema = z.object({
+  teamId: z.number(),
+  problemId: z.number(),
+})
+
+const handler: BlitzApiHandler = async (req: BlitzApiRequest, res: BlitzApiResponse) => {
+  if (req.method !== "POST") {
+    res.status(405).json({
+      msg: "Method not allowed",
+    })
+    return
+  }
+
+  // Create a new task
+  try {
+    const args = EndpointSchema.parse(req.body)
+
+    const team = await db.team.findUnique({
+      where: {
+        id: args.teamId,
+      },
+      rejectOnNotFound: true,
+    })
+    const problem = await db.problem.findUnique({
+      where: {
+        id: args.problemId,
+      },
+      rejectOnNotFound: true,
+    })
+
+    // This part needs to be CTF-specific
+    // We hardcode it for now
+    res.status(200).json({
+      host: `10.10.0.${team.aux}`,
+      port: problem.aux,
+    })
+  } catch (err) {
+    handleError(err, res)
+  }
+}
+
+export default handler
