7a88befc7acbafee0d2bd456b3416d30195f8946
Yechan Bae <yechan@gatech.edu>
2021-08-07 03:27:47 -0400

    Order problems by name

--- app/queries/getAllProblems.ts
+++ app/queries/getAllProblems.ts
@@ -2,6 +2,10 @@ import { Ctx } from "blitz"
 import db from "db"
 
 export default async function getAllProblems(input: any, ctx: Ctx) {
-  const problems = await db.problem.findMany()
+  const problems = await db.problem.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  })
   return problems
 }
